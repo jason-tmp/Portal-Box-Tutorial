cb1500dc7c893af886c9de6c9dfb5c94086aa7f5
Jason Deng <jason.zhu.deng@gmail.com>
2025-11-09 09:59:33 -0600

    Initial Commit

---
+++ Portal Box Tutorial/Portal_Box_TutorialApp.swift
@@ -0,0 +1,33 @@
+//
+//  Portal_Box_TutorialApp.swift
+//  Portal Box Tutorial
+//
+//  Created by Jason Deng on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Portal_Box_TutorialApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
---
+++ Portal Box TutorialTests/Portal_Box_TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  Portal_Box_TutorialTests.swift
+//  Portal Box TutorialTests
+//
+//  Created by Jason Deng on 11/9/25.
+//
+
+import Testing
+@testable import Portal_Box_Tutorial
+
+struct Portal_Box_TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

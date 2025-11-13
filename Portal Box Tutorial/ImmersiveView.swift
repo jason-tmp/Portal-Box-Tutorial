00e41f7a95f1e86529d4ef1e1b59665d590366ed
Jason Deng <jason.zhu.deng@gmail.com>
2025-11-12 19:09:28 -0600

    attach 4 portals to box

--- Portal Box Tutorial/ImmersiveView.swift
+++ Portal Box Tutorial/ImmersiveView.swift
@@ -32,16 +32,36 @@ struct ImmersiveView: View {
                     fatalError()
                 }
                 self.box = box
-                box.position = [1, 1, -1.5]  // meters
+                box.position = [1, 1, 1.5]  // meters
                 box.scale *= [0.5, 1, 0.5]
 
                 // Make world 1
                 let world1 = Entity()
                 world1.components.set(WorldComponent())
-
                 let skybox1 = await createSkyboxEntity(texture: "skybox1")
                 world1.addChild(skybox1)
                 content.add(world1)
+
+                // Make world 2
+                let world2 = Entity()
+                world2.components.set(WorldComponent())
+                let skybox2 = await createSkyboxEntity(texture: "skybox2")
+                world2.addChild(skybox2)
+                content.add(world2)
+
+                // Make world 3
+                let world3 = Entity()
+                world3.components.set(WorldComponent())
+                let skybox3 = await createSkyboxEntity(texture: "skybox3")
+                world3.addChild(skybox3)
+                content.add(world3)
+
+                // Make world 4
+                let world4 = Entity()
+                world4.components.set(WorldComponent())
+                let skybox4 = await createSkyboxEntity(texture: "skybox4")
+                world4.addChild(skybox4)
+                content.add(world4)
 
                 // Make portal 1
                 let world1Portal = createPortal(target: world1)
@@ -60,6 +80,58 @@ struct ImmersiveView: View {
                     angle: .pi / 2,
                     axis: [1, 0, 0]
                 )
+
+                // Make portal 2
+                let world2Portal = createPortal(target: world2)
+                content.add(world2Portal)
+
+                guard
+                    let anchorPortal2 = immersiveContentEntity.findEntity(
+                        named: "AnchorPortal_2"
+                    )
+                else {
+                    fatalError("Cannot find portal anchor")
+                }
+
+                anchorPortal2.addChild(world2Portal)
+                world2Portal.transform.rotation = simd_quatf(
+                    angle: -.pi / 2,
+                    axis: [1, 0, 0]
+                )
+
+                // Make portal 3
+                let world3Portal = createPortal(target: world3)
+                content.add(world3Portal)
+
+                guard
+                    let anchorPortal3 = immersiveContentEntity.findEntity(
+                        named: "AnchorPortal_3"
+                    )
+                else {
+                    fatalError("Cannot find portal anchor")
+                }
+
+                anchorPortal3.addChild(world3Portal)
+                let portal3RotX = simd_quatf(angle: .pi / 2, axis: [1, 0, 0])
+                let portal3RotY = simd_quatf(angle: .pi / 2, axis: [0, 1, 0])
+                world3Portal.transform.rotation = portal3RotY * portal3RotX
+
+                // Make portal 4
+                let world4Portal = createPortal(target: world4)
+                content.add(world4Portal)
+
+                guard
+                    let anchorPortal4 = immersiveContentEntity.findEntity(
+                        named: "AnchorPortal_4"
+                    )
+                else {
+                    fatalError("Cannot find portal anchor")
+                }
+
+                anchorPortal4.addChild(world4Portal)
+                let portal4RotX = simd_quatf(angle: .pi / 2, axis: [1, 0, 0])
+                let portal4RotY = simd_quatf(angle: -.pi / 2, axis: [0, 1, 0])
+                world4Portal.transform.rotation = portal4RotY * portal4RotX
 
             }
         }
